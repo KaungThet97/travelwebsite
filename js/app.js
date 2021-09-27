3b82f1d0bac35dd3604423e351d2a4c26c5c981b
KaungThet97 <thetkaung17@gmail.com>
2021-09-27 23:03:49 +0630

    test

---
+++ js/app.js
@@ -0,0 +1,34 @@
+// UI
+const codes = document.querySelectorAll('.code');
+// console.log(codes[0]);
+// codes[0].focus();
+
+codes.forEach((code,index)=>{
+    // console.log(code);
+
+    code.addEventListener('keydown',(e)=>{
+        
+        if(e.key >=0 && e.key <=9){
+            // codes[index++].focus();
+
+            // for only one number
+            codes[index].value='';  
+            if(index !== 5){
+                // codes[index+1].focus();
+
+                setTimeout(()=>{
+                    codes[index+1].focus(); 
+                },10)
+            }
+        }else if(e.key ==="Backspace"){
+            
+            codes[index-1].focus();
+            // if(index !== 0){
+            //     setTimeout(()=>codes[index-1].focus(),10);
+            // }
+             
+        }
+    })
+});
+
+// 8vb
